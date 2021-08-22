719497c41bd2b8459c1eb84745e1ffa7bf35b280
JoHnTsIm <84143748+JoHnTsIm@users.noreply.github.com>
2021-08-22 04:25:04 +0300

    Many Changes
    1. add some css
    
    2. organize the posts
    
    3. add search but dosent work, at least for now

---
+++ search.js
@@ -0,0 +1,5 @@
+function results() {
+    var searchResults = document.getElementById("search-results");
+    if (searchResults.style.display === "none") {
+        searchResults.style.display = "block"; }
+}
