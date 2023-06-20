9a0929b5458a7e6df8d770d59cc53231483c5f07
JoHnTsIm <84143748+JoHnTsIm@users.noreply.github.com>
2023-06-20 19:59:47 +0300

    add new javascript project

---
+++ javascript/todos/script.ts
@@ -0,0 +1,101 @@
+const toDoList = document.getElementById("todo-list");
+const toDoInput = document.getElementsByTagName("input")[0];
+
+// focus to the todo input
+toDoInput.focus();
+
+function addToDo() {
+    let toDoInputVal = toDoInput.value;
+    
+    if (toDoInputVal.length > 0) {
+
+        // creates a 'li' element
+        const todoLi = document.createElement('li');
+
+        // creates a 'button' element
+        const removeButton = document.createElement('button');
+
+        // creates a 'button' element
+        const doneButton = document.createElement('button');
+
+        // set class attribute of the button to 'fa fa-trash-o remove_button'
+        removeButton.setAttribute('class', 'fa fa-trash-o remove_button')
+
+        // set class attribute of the button to 'fa-check check_button'
+        doneButton.setAttribute('class', 'fa fa-check check_button')
+
+        // changes the text of the 'li' element to the text that is currently typed inside the input
+        todoLi.textContent = `${toDoInputVal} `;
+
+        // append/adds the 'li' element to the list, to the node
+        toDoList?.appendChild(todoLi);
+
+        // append/adds the 'button' element to the 'li', to the node
+        todoLi.appendChild(removeButton);
+
+        // append/adds the 'button' element to the 'li', to the node
+        todoLi.appendChild(doneButton);
+
+        // set class attribute of the button to 'list_li'
+        todoLi.setAttribute('class', 'list_li');
+
+        // adds event listener to the 'Remove' button of the current todo
+        // with functionality to remove the intire 'li' on the same line
+        // focus to the todo input
+        removeButton.addEventListener("click", () => 
+        {
+            todoLi.remove(); 
+            toDoInput.focus();
+        });
+
+        // adds event listener to the 'Done' button of the current todo
+        // with functionality to chnage the 'Done' button with 'Undo' button and the css of the 'li' element
+        // focus to the todo input
+        doneButton.addEventListener("click", () => 
+        {
+            if (todoLi.getAttribute('class') === 'list_li done_li') {
+                todoLi.setAttribute('class', 'list_li');
+                doneButton.setAttribute('class', 'fa fa-check check_button');
+                toDoInput.focus();
+            } else {
+                todoLi.setAttribute('class', 'list_li done_li');
+                doneButton.setAttribute('class', 'fa fa-undo undo_button');
+                toDoInput.focus();
+            }
+        }
+        );
+        
+        // clears the todo input
+        toDoInput.value = "";
+        toDoInput.focus();
+    };
+}
+
+// clears the todo input when 'Clear' button gets clicked
+// focus to the todo input
+function clearTodoInput() {
+    toDoInput.value = "";
+    toDoInput.focus();
+}
+
+// removes all the todo childs from the 'toDoList' element
+// focus to the input element
+function clearTodoList() {
+    if (toDoList != null) {
+        toDoList.replaceChildren();
+        toDoInput.focus();
+    }
+}
+
+
+
+// Keyboard Shortcuts
+document.addEventListener('keydown', (e) => {
+    if (e.key.toLowerCase() === 'a' && e.altKey) {
+        addToDo();
+    } else if (e.key.toLowerCase() === 'c' && e.altKey) {
+        clearTodoInput();
+    } else if (e.key.toLowerCase() === 'x' && e.altKey) {
+        clearTodoList();
+    }
+});
