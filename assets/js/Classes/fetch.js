2a949802088be38e383d08f1ca2b26c2eedf1514
JoHnTsIm <84143748+JoHnTsIm@users.noreply.github.com>
2025-08-25 23:19:02 +0300

    small fix oti api url

--- assets/js/Classes/fetch.js
+++ assets/js/Classes/fetch.js
@@ -11,7 +11,7 @@ export class Fetch {
     constructor() {
         this.file = "";
         this.action = "";
-        this.url = "https://crane-capital-buck.ngrok-free.app/test/public/api/";
+        this.url = "https://crane-capital-buck.ngrok-free.app/public/api/";
     }
     // * ITEMS
     fetchAllItems() {
